144d6d62b511719905b0e76ee47793487e2c5fe2
ManoranjanJena24 <mjena@mitaoe.ac.in>
2023-08-23 23:46:37 +0530

    Commit 4
    Redirecting to dashboard only when login is successful

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login'
 import Register from './pages/register'
-// import Dashboard from './pages/Dashboard'
+import Dashboard from './pages/dashboard'
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} /> {/* Use 'element' prop here */}
-          <Route path="/register" element={<Register />} /> {/* Use 'element' prop here */}
-          {/* <Route path="/dashboard" element={<Dashboard />} /> Use 'element' prop here */}
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/register" element={<Register />} /> 
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </div>
---
+++ client/src/pages/dashboard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Dashboard = () =>{
+    return <h1>Helloo</h1>
+}
+
+export default Dashboard
