dc72bf952d9ec70462c8403fe9d95a8436263854
ManoranjanJena24 <mjena@mitaoe.ac.in>
2023-08-23 23:23:25 +0530

    Commit 2
    Integrated with MongoDB
    
    Added authentication to login
    
    Added users to the DB and handled registration with the same email twice

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,20 @@
-import {useState} from 'react'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login'
+import Register from './pages/register'
+// import Dashboard from './pages/Dashboard'
 
-function App() {
-  const [name,setName]=useState('')
-  const [email,setEmail]=useState('')
-  const [password,setPassword]=useState('')
-
-  async function registerUser(event) {  //this is the first time when our FE will connect with the BE 
-    event.preventDefault()
-
-    const response = await fetch('http://localhost:3001/api/register',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json',
-      },
-      body:JSON.stringify({
-        name,
-        email,
-        password,
-      }),
-    })
-
-    const data= await response.json()
-    console.log(data)
-  }
-
+const App = () => {
   return (
-  <div>
-    <h1>Register</h1>
-    <form onSubmit={registerUser}>
-      <input 
-        value={name}
-        onChange={(e)=>setName(e.target.value)}
-        type="text"
-        placeholder="Name"
-      />
-      <br />
-      <input 
-        value={email}
-        onChange={(e)=>setEmail(e.target.value)}
-        type="email"
-        placeholder="Email"
-      />
-      <br />
-      <input 
-        value={password}
-        onChange={(e)=>setPassword(e.target.value)}
-        type="password"
-        placeholder="Password"
-      />
-      <br />
-      <input type="submit" value="Register"/>
-    </form>
-  </div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} /> {/* Use 'element' prop here */}
+          <Route path="/register" element={<Register />} /> {/* Use 'element' prop here */}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> Use 'element' prop here */}
+        </Routes>
+      </Router>
+    </div>
   )
 }
-
-export default App;
+export default App
--- server/index.js
+++ server/index.js
@@ -1,20 +1,61 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/user.model')
 
 app.use(cors())
 app.use(express.json()) //tells express that we need to parse anything that we get in the body to a JSON
 
-app.get('/',(req,res)=>{
+// mongoose.connect('mongodb://localhost:27017/mernStack')
+mongoose.connect('mongodb://127.0.0.1:27017/VideoRecorder')
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+db.on('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+app.get('/', (req, res) => {
     res.send('hello world!')
 })
 
-// app.post('/api/register', async (req,res)=>{
-app.post('/api/register', (req,res)=>{
+app.post('/api/register', async (req, res) => {
     console.log(req.body)
-    res.json({status:'ok'})
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+        console.log(err)
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
+
 })
 
-app.listen(3001,()=>{
+app.post('/api/login', async (req, res) => {
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    })
+    if (user) {
+        // const token = jwt.sign({
+        //     name: req.body.name,
+        //     email: req.body.email,
+        // }, 'secret123')
+        return res.json({ status: 'ok', user: true })
+    } else {
+        return res.json({ status: 'error', user: false })
+    }
+})
+
+app.listen(3001, () => {
     console.log('Server started on 3001')
 })
