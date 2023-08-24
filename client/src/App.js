74043809a0493e5e54bb452305c26b0ec73d33d8
ManoranjanJena24 <mjena@mitaoe.ac.in>
2023-08-24 14:20:13 +0530

    minor changes done

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
+import Register from './pages/register'
+import Dashboard from './pages/dashboard'
 
 const App = () => {
   return (
@@ -12,9 +12,6 @@ const App = () => {
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Register />} /> 
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/register" element={<Register />} /> 
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
