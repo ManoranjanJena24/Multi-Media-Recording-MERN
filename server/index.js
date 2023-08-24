cdf6c3ecd643788d7ea1703c00e5284ea86d46a9
ManoranjanJena24 <mjena@mitaoe.ac.in>
2023-08-24 10:45:16 +0530

    Encrypted user password

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/user.model')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 app.use(cors())
 app.use(express.json()) //tells express that we need to parse anything that we get in the body to a JSON
@@ -28,10 +29,11 @@ app.get('/', (req, res) => {
 app.post('/api/register', async (req, res) => {
     console.log(req.body)
     try {
+        const newPassword = await bcrypt.hash(req.body.password,10)
         await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: newPassword
         })
         res.json({ status: 'ok' })
     } catch (err) {
@@ -44,8 +46,12 @@ app.post('/api/register', async (req, res) => {
 app.post('/api/login', async (req, res) => {
     const user = await User.findOne({
         email: req.body.email,
-        password: req.body.password,
     })
+
+    if(!user){ return{ status: 'error', error: 'invalid login' }}
+
+    const isPassowrdValid =  await bcrypt.compare(req.body.password, user.password)
+
     if (user) {
         const token = jwt.sign(
             {
